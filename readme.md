a086b89364f3399f315bf683d24b3a2d90369dba
Evan Simmons <esims89@gmail.com>
2020-04-14 12:57:24 -0700

    Add readme and func

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Literally
+
+This is basically the `maplit` crate but with nicer names, and inputs are `.into()`'d. 
+
+Enjoy.
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,84 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+//! For when you just want a literal.
+
+/// Literally just a HashMap literal with keys and values into'd.
+/// ```rust
+/// let m = HashMap<String, String> = hmap!{ "key" => "value" };
+/// ```
+///
+#[macro_export(local_inner_macros)]
+macro_rules! hmap {
+    (@single $($x:tt)*) => (());
+    (@count $($rest:expr),*) => (<[()]>::len(&[$(hmap!(@single $rest)),*]));
+
+    ($($key:expr => $value:expr),* $(,)?) => {
+        {
+            let _cap = map!(@count $($key),*);
+            let mut _map = ::std::collections::HashMap::with_capacity(_cap);
+            $(
+                let _ = _map.insert($key.into(), $value.into());
+            )*
+            _map
+        }
+    };
+}
+
+/// Literally just a HashSet literal with values into'd.
+/// ```rust
+/// let s = HashSet<String> = hset!{ "value" };
+/// ```
+///
+#[macro_export(local_inner_macros)]
+macro_rules! hset {
+    (@single $($x:tt)*) => (());
+    (@count $($rest:expr),*) => (<[()]>::len(&[$(hset!(@single $rest)),*]));
+
+    ($($key:expr,)+) => { hset!($($key),+) };
+    ($($key:expr),*) => {
+        {
+            let _cap = hset!(@count $($key),*);
+            let mut _set = ::std::collections::HashSet::with_capacity(_cap);
+            $(
+                let _ = _set.insert($key.into());
+            )*
+            _set
+        }
+    };
+}
+
+/// Literally just a BTreeMap literal with keys and values into'd.
+/// ```rust
+/// let m = BTreeMap<String, String> = bmap!{ "key" => "value" };
+/// ```
+///
+#[macro_export(local_inner_macros)]
+macro_rules! bmap {
+    ( $($key:expr => $value:expr),* $(,)?) => {
+        {
+            let mut _map = ::std::collections::BTreeMap::new();
+            $(
+                let _ = _map.insert($key.into(), $value.into());
+            )*
+            _map
+        }
+    };
+}
+
+/// Literally just a HashSet literal with values into'd.
+/// ```rust
+/// let s = BTreeSet<String> = bset!{ "value" };
+/// ```
+///
+#[macro_export(local_inner_macros)]
+macro_rules! bset {
+    ($($key:expr,)+) => (btreeset!($($key),+));
+
+    ( $($key:expr),* ) => {
+        {
+            let mut _set = ::std::collections::BTreeSet::new();
+            $(
+                _set.insert($key.into());
+            )*
+            _set
+        }
+    };
 }
